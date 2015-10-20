0b3ca6c78a27ba6bf728292b957f7761fa9cf4af
tigrex22 <tigrex0806@gmail.com>
2015-10-20 23:01:52 +0200

    libft-v0.4

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,26 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pforgaci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/10/20 19:30:04 by pforgaci          #+#    #+#             */
-/*   Updated: 2015/10/20 23:00:32 by pforgaci         ###   ########.fr       */
+/*   Created: 2015/10/20 22:57:33 by pforgaci          #+#    #+#             */
+/*   Updated: 2015/10/20 23:00:03 by pforgaci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-int		main(int ac, char **av)
+size_t	ft_strlen(const char *s)
 {
-	(void)ac;
-	(void)av;
-	printf("%d\n", ft_strlen(av[1]));
-	printf("%d\n", strlen(av[1]));
-	return (0);
+	int i;
+
+	i = 0;
+	while (s[i])
+		++i;
+	return (i);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pforgaci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/20 19:34:23 by pforgaci          #+#    #+#             */
-/*   Updated: 2015/10/20 22:29:56 by pforgaci         ###   ########.fr       */
+/*   Updated: 2015/10/20 23:01:28 by pforgaci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n); //
 void	*ft_memmove(void *dst, const void *src, size_t len); //
 void	*ft_memchr(const void *s, int c, size_t n); //
 int		ft_memcmp(const void *s1, const void *s2, size_t n); //
-size_t	ft_strlen(const char *s); //
+size_t	ft_strlen(const char *s); //?
 char	*ft_strdup(const char *s1); //
 char	*ft_strcpy(char *dst, const char *src); //
 char	*ft_strncpy(char *dst, const char *src, size_t n); //
