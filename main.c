c5025e6d880ccbb935e900eaf851284d72116b7a
tigrex22 <tigrex0806@gmail.com>
2015-10-26 17:17:05 +0200

    libft-v0.5

--- main.c
+++ main.c
@@ -15,12 +15,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <unistd.h>
 
 int		main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-	printf("%d\n", ft_strlen(av[1]));
-	printf("%d\n", strlen(av[1]));
+	//printf("%d\n", ft_strlen(av[1]));
+	//printf("%d\n", strlen(av[1]));
+	write(1, av[1], 8);
+	write(1, "\n", 1);
+	ft_memcpy(av[1], av[2], 3);
+	write(1, av[1], 8);
+	write(1, "\n", 1);
+	//write(1, av[2], 8);
+	//write(1, "\n", 1);
+	//memcpy(av[2], '$', 3);
+	//write(1, av[2], 8);
+	//write(1, "\n", 1);
 	return (0);
 }
